d7d619bfbca9ef71c3b4a3f7ec73fea5fd6a4eed
wdsalih677 <98893919+wdsalih677@users.noreply.github.com>
2023-03-19 09:53:18 -0700

    patients final data

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -58,7 +58,7 @@
                     <td>{{ $tik->ticket_num }}</td>
                     <td>
                         @if (!empty($tik->pre_diagnoses->provisional_diagnosis))
-                            <h4><span class="badge badge-success">{{ $tik->pre_diagnoses->provisional_diagnosis }}</span></h4>
+                            <h4><span class="badge badge-warning">{{ $tik->pre_diagnoses->provisional_diagnosis }}</span></h4>
                         @else
                             <h4><span class="badge badge-danger">المريض تحت التشخيص</span></h4>
                         @endif
@@ -103,10 +103,12 @@
                                 @else
                                     <h4><span class="badge badge-danger">المريض تحت الفحص</span></h4>
                                 @endif
-                                <button class="dropdown-item" data-reg_id="" data-toggle="modal" data-target="#dS" ><i
-                                    class="text-danger fa fa-trash"></i>
-                                    حذف
-                                </button>
+                                @if (empty($tik->patientsfinaldata->id))
+                                    <button class="dropdown-item" data-reg_id="" data-toggle="modal" data-target="#dS" ><i
+                                        class="text-danger fa fa-trash"></i>
+                                        حذف
+                                    </button>
+                                @endif
                                 <button class="dropdown-item" data-reg_id="" data-toggle="modal" data-target="#addStudent" >
                                     <i class="text-info fa fa-print"></i>
                                     طباعه
@@ -170,93 +172,92 @@
                             </div>
                         </div>
                         <!-- نهاية تعديل التشخيص الإبتدائي -->
-                    @else
-                        <h4><span class="badge badge-danger">المريض تحت التشخيص</span></h4>
                     @endif
                     {{-- ==================================================================================================================================================================================================== --}}
                     @if (!empty($tik->patientsfinaldata->id))
                         <!-- بداية تعديل التشخيص النهائي -->
-                            <div class="modal fade bd-example-modal-lg" id="edit_two{{ $tik->patientsfinaldata->id }}" tabindex="-1" role="dialog"
-                                aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-lg" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title"
-                                                id="exampleModalLabel">
-                                                تعديل التشخيص النهائي
-                                            </h5>
-                                            <button type="button" class="close" data-dismiss="modal"
-                                                aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <!-- edit_form -->
-                                            <form action="{{ route('final_data.update',$tik->patientsfinaldata->id) }}" method="POST">
-                                                {{ method_field('patch') }}
-                                                @csrf
-                                                <input type="hidden" name="id" value="{{ $tik->patientsfinaldata->id }}">
-                                                <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title">إسم الريض : {{ $tik->name }}</h5>
-                                                <br>
-                                                <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title">رقم التذكره : {{ $tik->ticket_num }}</h5>
-                                                <br>
-                                                <div class="row">
-                                                    <div class="col-md-6 mb-30">
-                                                        <div class="mb-3">
-                                                            <label class="form-label" for="exampleInputEmail1">التشخيص النهائي | Final Diagnosis :</label>
-                                                            <input id="name" type="فثءف" name="final_diagnosis" class="form-control" autocomplete="off" value="{{ $tik->patientsfinaldata->final_diagnosis }}">
-                                                        </div>
-                                                        <div class="mb-3">
-                                                            <label class="form-label" for="exampleFormControlSelect1">العنبر :</label>
-                                                            <select class="form-control" name="ward_id" id="exampleFormControlSelect1" style="height: 50px;">
-                                                                @foreach ($wards as $ward)
-                                                                    <option value="{{ $ward->id }}" {{ $tik->patientsfinaldata->ward_id == $ward->id ? 'selected' :'' }}>{{ $ward->name }}</option>
-                                                                @endforeach
-                                                            </select>
-                                                        </div>
-                                                        <div class="mb-3">
-                                                            <label class="form-label" for="exampleFormControlSelect1">القسم :</label>
-                                                            <select class="form-control" name="section_id" id="exampleFormControlSelect1" style="height: 50px;">
-                                                                @foreach ($sections as $section)
-                                                                    <option value="{{ $section->id }}" {{ $tik->patientsfinaldata->section_id == $section->id ? 'selected' :'' }}>{{ $section->name }}</option>
-                                                                @endforeach
-                                                            </select>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-6 mb-30">
-                                                        <div class="mb-3">
-                                                            <label class="form-label" for="exampleFormControlSelect1">نوع الإقامه :</label>
-                                                            <select class="form-control" name="residence_type" id="exampleFormControlSelect1" style="height: 50px;">
-                                                                <option value="1" {{ $tik->patientsfinaldata->residence_type == 1 ? 'selected' : '' }}>لا يوجد</option>
-                                                                <option value="2" {{ $tik->patientsfinaldata->residence_type == 2 ? 'selected' : '' }}>إقامه طويله</option>
-                                                                <option value="3" {{ $tik->patientsfinaldata->residence_type == 3 ? 'selected' : '' }}>إقامه قصيره</option>
-                                                            </select>
-                                                        </div>
-                                                        <div class="mb-3">
-                                                            <label class="form-label" for="exampleInputEmail1">تاريخ المقابله :</label>
-                                                            <div class="input-group" data-date="23/11/2018" data-date-format="mm/dd/yyyy">
-                                                                <input type="text" name="follow_up_date" class="form-control range-to"   data-date-format="mm/dd/yyyy" value="{{ $tik->patientsfinaldata->follow_up_date == NULL ? 'لا يوجد تاريخ مقابله' : $tik->patientsfinaldata->follow_up_date }}">
-                                                            </div>
-                                                        </div>
-                                                        <div class="mb-3">
-                                                            <label class="form-label" for="exampleFormControlTextarea1">العلاج و التغذيه | Treatment & Diet</label>
-                                                            <textarea class="form-control" name="treatment_diet" id="exampleFormControlTextarea1" style="height: 50px;">{{ $tik->patientsfinaldata->treatment_diet }}</textarea>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                        </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary"
-                                                        data-dismiss="modal">اغلاق</button>
-                                                    <button type="submit" class="btn btn-success">تحديث</button>
-                                                </div>
-                                        </form>
+                        <div class="modal fade bd-example-modal-lg" id="edit_two{{ $tik->patientsfinaldata->id }}" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title"
+                                            id="exampleModalLabel">
+                                            تعديل التشخيص النهائي
+                                        </h5>
+                                        <button type="button" class="close" data-dismiss="modal"
+                                            aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
                                     </div>
+                                    <div class="modal-body">
+                                        <!-- edit_form -->
+                                        <form action="{{ route('final_data.update',$tik->patientsfinaldata->id) }}" method="POST">
+                                            {{ method_field('patch') }}
+                                            @csrf
+                                            <input type="hidden" name="id" value="{{ $tik->patientsfinaldata->id }}">
+                                            <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title">إسم الريض : {{ $tik->name }}</h5>
+                                            <br>
+                                            <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title">رقم التذكره : {{ $tik->ticket_num }}</h5>
+                                            <br>
+                                            <div class="row">
+                                                <div class="col-md-6 mb-30">
+                                                    <div class="mb-3">
+                                                        <label class="form-label" for="exampleInputEmail1">التشخيص النهائي | Final Diagnosis :</label>
+                                                        <input id="name" type="فثءف" name="final_diagnosis" class="form-control" autocomplete="off" value="{{ $tik->patientsfinaldata->final_diagnosis }}">
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label class="form-label" for="exampleFormControlSelect1">العنبر :</label>
+                                                        <select class="form-control" name="ward_id" id="exampleFormControlSelect1" style="height: 50px;">
+                                                            <option value="" {{ $tik->patientsfinaldata->ward_id == NULL ? 'selected' :'' }}>لا يوجد</option>
+                                                            @foreach ($wards as $ward)
+                                                                <option value="{{ $ward->id }}" {{ $tik->patientsfinaldata->ward_id == $ward->id ? 'selected' :'' }}>{{ $ward->name }}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label class="form-label" for="exampleFormControlSelect1">القسم :</label>
+                                                        <select class="form-control" name="section_id" id="exampleFormControlSelect1" style="height: 50px;">
+                                                            <option value="" {{ $tik->patientsfinaldata->section_id == NULL ? 'selected' :'' }}>لا يوجد</option>
+                                                            @foreach ($sections as $section)
+                                                                <option value="{{ $section->id }}" {{ $tik->patientsfinaldata->section_id == $section->id ? 'selected' :'' }}>{{ $section->name }}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6 mb-30">
+                                                    <div class="mb-3">
+                                                        <label class="form-label" for="exampleFormControlSelect1">نوع الإقامه :</label>
+                                                        <select class="form-control" name="residence_type" id="exampleFormControlSelect1" style="height: 50px;">
+                                                            <option value="1" {{ $tik->patientsfinaldata->residence_type == 1 ? 'selected' : '' }}>لا يوجد</option>
+                                                            <option value="2" {{ $tik->patientsfinaldata->residence_type == 2 ? 'selected' : '' }}>إقامه طويله</option>
+                                                            <option value="3" {{ $tik->patientsfinaldata->residence_type == 3 ? 'selected' : '' }}>إقامه قصيره</option>
+                                                        </select>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label class="form-label" for="exampleInputEmail1">تاريخ المقابله :</label>
+                                                        <div class="input-group" data-date="23/11/2018" data-date-format="mm/dd/yyyy">
+                                                            <input type="text" name="follow_up_date" class="form-control range-to"   data-date-format="mm/dd/yyyy" value="{{ $tik->patientsfinaldata->follow_up_date == NULL ? 'لا يوجد تاريخ مقابله' : $tik->patientsfinaldata->follow_up_date }}">
+                                                        </div>
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label class="form-label" for="exampleFormControlTextarea1">العلاج و التغذيه | Treatment & Diet</label>
+                                                        <textarea class="form-control" name="treatment_diet" id="exampleFormControlTextarea1" style="height: 50px;">{{ $tik->patientsfinaldata->treatment_diet }}</textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                    </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-dismiss="modal">اغلاق</button>
+                                                <button type="submit" class="btn btn-success">تحديث</button>
+                                            </div>
+                                    </form>
                                 </div>
                             </div>
-                            <!-- نهاية تعديل التشخيص النهائي -->
-                    @else
-                        <h4><span class="badge badge-danger">المريض تحت الفحص</span></h4>
+                        </div>
+
+                        <!-- نهاية تعديل التشخيص النهائي -->
                     @endif
                 @endforeach
               </tbody>
