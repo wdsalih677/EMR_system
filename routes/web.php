524ab679e59bc968c695a4039e59f18fb96f4f65
wdsalih677 <98893919+wdsalih677@users.noreply.github.com>
2023-12-18 07:14:20 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -61,55 +61,55 @@ Route::group(['middleware' => ['auth']], function() {
 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');//الصفحه الرئيسيه
 
-Route::resource('doctor', DoctorController::class);//الأطباء
-
-Route::resource('section', SectionController::class);//الاقسام
-
-Route::resource('ward', WardController::class);//العنابر
-
-Route::resource('reception', ReceptionController::class);//الإستقبال
-
-Route::resource('patient', PatientController::class);//ادارة المرضى
-
-Route::get('/search', [PatientController::class , 'search'])->name('search_teckit');//مجهول
-
-Route::resource('final_data', PatientFinalDataController::class);//بيانات المريض النهائيه
-
-Route::resource('intensive_care', IntensiveCareController::class);
-
-Route::resource('operation', OperationController::class);//العمليات
-
-Route::resource('examination', ExaminationsController::class);//الفحوصات
-
-Route::resource('patient_follow_up', PatientFollowUpController::class);//متابعة المريض
-
-Route::resource('medical_record' ,MedicalRecordController::class);//السجل الطبي
-
-Route::resource('report', ReportController::class);//التقارير
-
-Route::resource('operation_report', OperationReportController::class);//تقارير العمليات
-
-Route::resource('diseases_report', DiseasesReportController::class);//تقارير الامراض
-
-Route::resource('Communicable' ,CommunicableReportController::class);
-
-Route::resource('nonCommunicable' ,NnCommunicableReportController::class);
-
-Route::resource('birth' ,BirthController::class);//المواليد
-
-Route::resource('death', DeathController::class);// الوفيات
-
-Route::resource('birthDeathReport', BirthDeathReportController::class);//تقرير المواليد و الوفيات
-
-Route::resource('birthArchive', BirthArchiveController::class);//ارشيف المواليد
-
-Route::resource('OperationAttachment' , OperationAttachmentController::class);//مرفقات العمليه
-
-Route::get('openFile/{teckit_number}/{file_name}',[OperationAttachmentController::class , 'openFile'])->name('openFile');//عرض الإقرار الطبي
-
-Route::get('downloadFile/{invoice_number}/{file_name}',[OperationAttachmentController::class , 'downloadFile']);//تحميل الإقرار الطبي
-
-Route::resource('biometrics', BiometricsController::class);//العلامات الحيويه
-Route::resource('BioFollow', BioFollowUpController::class);//العلامات الحيويه للمتابعه
+    Route::resource('doctor', DoctorController::class);//الأطباء
+    
+    Route::resource('section', SectionController::class);//الاقسام
+    
+    Route::resource('ward', WardController::class);//العنابر
+    
+    Route::resource('reception', ReceptionController::class);//الإستقبال
+    
+    Route::resource('patient', PatientController::class);//ادارة المرضى
+    
+    Route::get('/search', [PatientController::class , 'search'])->name('search_teckit');//مجهول
+    
+    Route::resource('final_data', PatientFinalDataController::class);//بيانات المريض النهائيه
+    
+    Route::resource('intensive_care', IntensiveCareController::class);
+    
+    Route::resource('operation', OperationController::class);//العمليات
+    
+    Route::resource('examination', ExaminationsController::class);//الفحوصات
+    
+    Route::resource('patient_follow_up', PatientFollowUpController::class);//متابعة المريض
+    
+    Route::resource('medical_record' ,MedicalRecordController::class);//السجل الطبي
+    
+    Route::resource('report', ReportController::class);//التقارير
+    
+    Route::resource('operation_report', OperationReportController::class);//تقارير العمليات
+    
+    Route::resource('diseases_report', DiseasesReportController::class);//تقارير الامراض
+    
+    Route::resource('Communicable' ,CommunicableReportController::class);
+    
+    Route::resource('nonCommunicable' ,NnCommunicableReportController::class);
+    
+    Route::resource('birth' ,BirthController::class);//المواليد
+    
+    Route::resource('death', DeathController::class);// الوفيات
+    
+    Route::resource('birthDeathReport', BirthDeathReportController::class);//تقرير المواليد و الوفيات
+    
+    Route::resource('birthArchive', BirthArchiveController::class);//ارشيف المواليد
+    
+    Route::resource('OperationAttachment' , OperationAttachmentController::class);//مرفقات العمليه
+    
+    Route::get('openFile/{teckit_number}/{file_name}',[OperationAttachmentController::class , 'openFile'])->name('openFile');//عرض الإقرار الطبي
+    
+    Route::get('downloadFile/{invoice_number}/{file_name}',[OperationAttachmentController::class , 'downloadFile']);//تحميل الإقرار الطبي
+    
+    Route::resource('biometrics', BiometricsController::class);//العلامات الحيويه
+    Route::resource('BioFollow', BioFollowUpController::class);//العلامات الحيويه للمتابعه
 
 });
